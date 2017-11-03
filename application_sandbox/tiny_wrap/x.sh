6cc41395d6550e87086ee89e4c2670e3ff8379fd
David Neto <dneto@google.com>
2017-11-03 14:39:47 -0400

    Comment the x.sh script

--- application_sandbox/tiny_wrap/x.sh
+++ application_sandbox/tiny_wrap/x.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Conversion script from OpenCL C to SPIR-V assembly.
 clspv copy_struct.cl -hack-undef -o a0.spv
 spirv-opt --inline-entry-points-exhaustive --eliminate-dead-functions a0.spv -o a.spv
 spirv-dis a.spv >a.spvasm
+
+# Then copy a.spvasm to copy_struct.spvasm
+# Optional: Edit copy_struct.spvasm
